bbb08d22850f6ca1499629dc951f96fa5e8c2157
IvanStukalov <ivanstukalov2003@gmail.com>
2023-06-25 18:20:30 +0300

    2622-cache-with-time-limit

---
+++ 2622-cache-with-time-limit.js
@@ -0,0 +1,62 @@
+// Write a class that allows getting and setting key-value pairs,
+//  however a time until expiration is associated with each key.
+//
+// The class has three public methods:
+//
+// set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds.
+//  Once the duration has elapsed, the key should be inaccessible.
+//  The method should return true if the same un-expired key already exists and false otherwise.
+//  Both the value and duration should be overwritten if the key already exists.
+//
+// get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
+//
+// count(): returns the count of un-expired keys.
+
+var TimeLimitedCache = function() {
+  this.cache = {};
+  this.timeout = 0;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+  let wasField = false;
+  if (this.cache[key]) {
+    wasField = true;
+    clearTimeout(this.timeout);
+  }
+  this.cache[key] = value;
+  this.timeout = setTimeout(() => {
+    delete this.cache[key];
+  }, duration);
+  return wasField;
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+  if (this.cache[key] === null) {
+    return -1;
+  }
+  return this.cache[key];
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+  return Object.keys(this.cache).length;
+};
+
+
+// Your TimeLimitedCache object will be instantiated and called as such:
+var obj = new TimeLimitedCache()
+console.log(obj.set(1, 42, 1000)); // false
+console.log(obj.get(1)) // 42
+console.log(obj.count()) // 1
